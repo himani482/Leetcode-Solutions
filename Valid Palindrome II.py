673ad290ce74246c3af7bd726b5d362e44bcdc99
HIMANI BHAISORA <63862587+himani482@users.noreply.github.com>
2026-04-08 21:25:44 +0530

    Create Valid Palindrome II.py

---
+++ Valid Palindrome II.py
@@ -0,0 +1,13 @@
+# Given a string s, return true if the s can be palindrome after deleting at most one character from it.
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        l, r = 0, len(s) - 1
+        while l < r:
+            
+            if not s[l] == s[r]:
+
+                return s[l+1:r+1] == s[l+1:r+1][::-1] or s[l:r] == s[l:r][::-1]
+            else:
+                l += 1
+                r -= 1
+        return True
