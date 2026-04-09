d3b63ca260c009af66f8fc16fa0db8578bf18266
HIMANI BHAISORA <63862587+himani482@users.noreply.github.com>
2026-04-09 20:32:23 +0530

    Create  Best Time to Buy and Sell Stock II.py

---
+++ Best Time to Buy and Sell Stock II.py
@@ -0,0 +1,18 @@
+# You are given an integer array prices where prices[i] is the price of a given stock on the ith day.
+
+# On each day, you may decide to buy and/or sell the stock. You can only hold at most one share of the stock at any time. However, you can sell and buy the stock multiple times on the same day, ensuring you never hold more than one share of the stock.
+
+# Find and return the maximum profit you can achieve.
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        profit =0
+        n = len(prices)
+        min_price  = prices[0]
+        for i in range(1,n):
+          
+            if prices[i ] > prices[i - 1] :
+                profit +=   prices[i ] - prices[i-1]
+
+        
+        return profit
